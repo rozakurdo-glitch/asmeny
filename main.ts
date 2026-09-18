72a814d908564506dde0ead1629fdced37bafd5c
rozakurdo-glitch <roza.kurdo@education.nsw.gov.au>
2026-09-18 10:08:35 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    d d d d d d f f f f d d d d d d 
+    d d d d f f f 3 3 f f f d d d d 
+    d d d f f f 3 3 3 3 f f f d d d 
+    d d f f f 9 9 9 9 9 9 f f f d d 
+    d d f 9 9 3 3 3 3 3 3 9 9 f d d 
+    d d f 9 3 3 f f f f f 3 9 f d d 
+    d d f f f f e e e e f f f f d d 
+    d f f e f 3 f b b f 3 f e f f d 
+    d f e e b b f d d f b b e e f d 
+    d d f e e d d d d d d e e f d d 
+    d d d f e e b b b b e e f d d d 
+    d d f f f 3 3 3 3 3 3 f f f d d 
+    d d f 9 f 3 3 3 3 3 3 f 9 f d d 
+    d d f f f b b 9 9 b b f f f d d 
+    d d d d d f f f f f f d d d d d 
+    d d d d d f f d d f f d d d d d 
+    `, SpriteKind.Player)
